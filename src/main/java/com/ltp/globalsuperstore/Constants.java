e485d42d3011b8001872aa8f01e03e9becf63289
EmJey <emjeydev@gmail.com>
2023-05-01 18:44:43 +0330

    add springboot bootcamp challenge, global_superstore project

---
+++ src/main/java/com/ltp/globalsuperstore/Constants.java
@@ -0,0 +1,11 @@
+package com.ltp.globalsuperstore;
+
+public class Constants {
+    public static final String[] CATEGORIES = new String[] {"Furniture", "Office Supplies", "Technology"};
+
+    public static final int NOT_FOUND = -1;
+
+    public static final String SUCCESS_STATUS = "Success";
+
+    public static final String FAILED_STATUS = "Failed";
+}
